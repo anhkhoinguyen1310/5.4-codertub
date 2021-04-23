d81b4dfd395c69adc6aade6b69d602a418ef06f4
anhkhoinguyen1310 <anhkhoinguyen1310@gmail.com>
2021-04-23 16:28:56 +0700

    add abit

--- src/App.js
+++ src/App.js
@@ -10,25 +10,26 @@ import Navbar from './components/Navbar'
 
 const API_KEY = process.env.REACT_APP_MOVIE_API_KEY
 function Homepage() {
-  const [Movies] = useState([])
+  const [movies, setMovies] = useState([])
+
   const fetchMovies = async () => {
-    const resp = await fetch (`https://api.themoviedb.org/3/movie/76341?api_key=${API_KEY}`,);
+    const resp = await fetch (`https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}`);
     const json = await resp.json()
     console.log({json})
+    setMovies(json.results);
   }
+
   useEffect(() => {
     fetchMovies()
   },[])
   return (
     <div> 
       <h1> Home Page</h1>
-      {
-        Movies.map(m => {
-          return <h1>{m.title}</h1>
-        })
-      }
-    </div>
-  )
+      {movies.map(m => {
+          return <h1>{m && m.title}</h1>
+        })}
+    </div> 
+  );
 }
 
 function App() {
