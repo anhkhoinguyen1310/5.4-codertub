7de838acb3eac03cb1959b7b45c3e32730ab6fa3
anhkhoinguyen1310 <anhkhoinguyen1310@gmail.com>
2021-04-23 15:42:03 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import { Link, useParams } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { Switch, Route } from "react-router-dom";
+import React from 'react'
+import { Button, Container, Row, Card } from 'react-bootstrap';
 import './App.css';
+import Navbar from './components/Navbar'
+
+
+const API_KEY = process.env.REACT_APP_MOVIE_API_KEY
+function Homepage() {
+  const [Movies, setMovies] = useState([])
+  const fetchMovies = async () => {
+    const resp = await fetch (`https://api.themoviedb.org/3/movie/76341?api_key=${API_KEY}`,);
+    const json = await resp.json()
+    console.log({json})
+  }
+  useEffect(() => {
+    fetchMovies()
+  },[])
+  return (
+    <div> 
+      <h1> Home Page</h1>
+      {
+        Movies.map(m => {
+          return <h1>{m.title}</h1>
+        })
+      }
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar />
+    <Container>
+      <Switch>
+      <Route path="/" exact component={Homepage} />
+      </Switch>
+    </Container>
+  </div>
   );
 }
 
