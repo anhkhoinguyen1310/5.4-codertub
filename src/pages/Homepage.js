5309ef8791bf885cbdee43430fa1b7cc855871ae
nhat khoa <nhatkhoa.tin@gmail.com>
2021-04-26 14:16:41 +0700

    add sidebar and bookdetail page

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,57 +1,127 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from 'react'
 import React from 'react'
-import { Container, Row, Col, Card, Nav } from 'react-bootstrap';
-import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardImage,
-    MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn } from "mdbreact";
+import SideBar from '../components/SideBar';
+import {  Row, Col, Card, Nav } from 'react-bootstrap';
+// import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardImage,
+    // MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn } from "mdbreact";
     
 const API_KEY = process.env.REACT_APP_MOVIE_API_KEY
 
 
 export default function Homepage() {
     const [movies, setMovies] = useState([])
+    const [genres, setGenres] = useState({genres: []});
+    const [gen_ids, setGen_ids] = useState([]);
+    const [moviesDefault, setMoviesDefault] = useState([]);
+    
+
 
     const fetchMovies = async () => {
       const resp = await fetch (`https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}`);
       const json = await resp.json()
-      console.log({json})
+      // console.log({json})
       setMovies(json.results);
+      setMoviesDefault(json.results);
     }
-  
+    
+    // Fetch genres's name
+    const fetchGenresMovie = async () => {
+        let url = `https://api.themoviedb.org/3/genre/movie/list?&api_key=${API_KEY}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        setGenres(data);
+    }
+
+
+function splitNumber(arr) {
+    var a = [],
+    b = [],
+    prev;
+
+    arr.sort();
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] !== prev) {
+        a.push(arr[i]);
+        b.push(1);
+        } else {
+        b[b.length - 1]++;
+        }
+        prev = arr[i];
+    }
+
+    return [a, b];
+}
+
+  const getCategory = (newG) => {
+        let g = moviesDefault.map(m => m.genre_ids);
+        let gdx = g.join().split(",").map(m => parseInt(m));
+        let p = splitNumber(gdx) ;
+        let results = [] ;
+        for(let i = 0; i< p[0].length; i++ ){
+            let h = {};
+            h.id = p[0][i];
+            h.count = p[1][i];
+            results.push(h);
+        }
+        for(let i of results){
+            for(let j of newG.genres){
+                if(i.id === j.id){
+                    i['name'] = j.name;
+                }
+            }
+        }
+        setGen_ids(results);
+    } 
+    
+    // console.log(genres)
+
     useEffect(() => {
       fetchMovies()
     },[])
+
+    useEffect(() => {
+      fetchGenresMovie();
+    }, [movies])
+
+
+    useEffect(() => {
+      getCategory(genres);
+    }, [genres])
+
+
     return (
-      <div> 
-        <h1> Home Page</h1>
-        <Container>
-    <Row>
-    {movies.map(m =>{
-      console.log({m});
-            return (
-                <>
-            <Col >
-            <Card className = "m-3"  style={{ width: '15rem' }}>
-            <Card.Img variant="top" src={'https://image.tmdb.org/t/p/w500/' + m.backdrop_path }/>
-            <Card.Body>
-              <Card.Title>{m.title}</Card.Title>
-              <Card.Text style = {{height: 200, overflow: 'hidden'}}>
-                {m.overview}
-              </Card.Text>
-              <Nav.Link as={Link} to= {'/movie/' + m.id}  className = "btn btn-primary">View Details</Nav.Link>
-         
-            </Card.Body>
-          </Card>
-          </Col>
+      <div className="main-content"> 
+        <div className="sidebar">
+            <SideBar moviesDefault={moviesDefault} movies={movies} setMovies={setMovies} gen_ids={gen_ids}  />
+        </div>
+        <div className="movies-content">
+          <h1 className="text-center"> Home Page</h1>
+          <div className="main-content">
+            <Row>
+            {movies.map(m =>{
+                    return (
+                        <>
+                    <Col >
+                    <Card className = "m-3"  style={{ width: '15rem' }}>
+                    <Card.Img variant="top" src={'https://image.tmdb.org/t/p/w500/' + m.backdrop_path }/>
+                    <Card.Body>
+                      <Card.Title>{m.title}</Card.Title>
+                      <Card.Text style = {{height: 200, overflow: 'hidden', overflowY: "auto"}}>
+                        {m.overview}
+                      </Card.Text>
+                      <Nav.Link as={Link} to= {'/movie/' + m.id}  className = "btn btn-primary">View Details</Nav.Link>   
+                    </Card.Body>
+                  </Card>
+                  </Col>
+                </>
+                  )
+                  })}
+              
+            </Row>
+          </div>
+        </div>
 
-
-           </>
-          )
-          })}
-      
-    </Row>
-  </Container>
-        
       </div> 
     )
 }
--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,9 +1,118 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import { useParams } from "react-router-dom";
+
+const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
+
 
 export default function MoviePage() {
+    const { id } = useParams();
+    const [movie, setMovie] = useState({ genres: [], production_companies: [] })
+    const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState("");
+
+    const fetchMovieData = async () => {
+        const url =`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY  }`;
+        const res = await fetch(url);
+        const json = await res.json();
+        setMovie(json);
+        fetchLocalComments(json);
+    }
+
+
+     const fetchLocalComments = (m) => {
+        const state = JSON.parse(localStorage.getItem('imdbState'));
+        if(state){
+            console.log({lalala: state})
+            const savedMovie = state.movies.find((m) => m.id === parseInt(id));
+            if(savedMovie){
+                setComments(savedMovie.comments)
+            } else {
+                m.comments = [];
+                state.movies =  [...state.movies, m];
+                localStorage.setItem("imdbState", JSON.stringify(state))
+            }
+        }
+    }
+
+    
+
+    const savedComment = (e) => {
+        e.preventDefault();
+        const state =  JSON.parse(localStorage.getItem('imdbState'));
+        const savedMovie = state.movies.find((m) => m.id === parseInt(id));
+        savedMovie.comments.push(comment);
+        const idx = state.movies.findIndex((m) => m.id === parseInt(id));
+        state.movies[idx] = savedMovie;
+        localStorage.setItem('imdbState', JSON.stringify(state));
+        if(comment !== ""){
+            setComments([...comments, comment])
+        }
+        setComment("");
+    }
+
+
+    
+    useEffect(() => {
+        fetchMovieData()
+    },[id])
+
     return (
-        <div>
-            <h1>Individual Movie</h1>
+        <div style={{height: "160vh"}}>
+            <h1 className="text-center">Movie Detail</h1>
+            <div className="d-flex  align-items-center">
+                <div className="container  card-movie" style={{height: "140vh"}}>
+                    <img className="img-content" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+                    <div className="text-content">
+                        <h1 className="text-center m-5">{movie.title}</h1>
+                        <div className="genders">
+                            <strong>Genres:</strong>  {movie.genres.map(g => <button key={g.id} className="genre-btn">{g.name}</button>)}
+                        </div>
+                        <div className="overview">
+                            <strong>Overview:</strong>  {movie.overview}
+                        </div>
+                        <div className="vote ">
+                            <div>
+                                <strong>Vote average:</strong> {movie.vote_average}
+                            </div>
+                            <div>
+                                <strong>Vote count:</strong> {movie.vote_count}
+                            </div>
+                        </div>
+                        <div className="budget">
+                            <strong>Budget:</strong>    {movie.budget} $
+                        </div>
+                        <div className="popuy">
+                            <strong>Popularity:</strong>{movie.popularity}
+                        </div>
+                        <strong>Production:</strong>
+                        <div className="production">
+                                {movie.production_companies.map((p) => 
+                                                p.logo_path === null ? 
+                                                (
+                                                    <p className="production-text">{p.name}</p>
+                                                ) : (
+                                                    <img src={`https://image.tmdb.org/t/p/w500/${p.logo_path}` } alt={p.name} />
+                                                ))}
+                        </div>
+                        <div className="form-comments">
+                            <div className="comment-list">
+                                {comments && comments.reverse().map(m => <p className="comment-item">{m}</p>)}
+                            </div>
+                            <form onSubmit={savedComment} className="input" >
+                                <input 
+                                    value={comment}
+                                    onChange={(e) => setComment(e.target.value)}
+                                    type="text" 
+                                    placeholder="comment" />
+                                <input 
+                                    onClick={savedComment}
+                                    type="submit" 
+                                    value="Enter" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
